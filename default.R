083b5d8b023e302a5391cd119f042358e59e250c
Destiny O'Neill <doneill1923@gmail.com>
2025-04-04 20:38:58 -0400

    - Made a Bar chart of Flight Phase strikes - Made a histogram of the height where planes were when the strike occrred - Got libraries loaded

--- default.R
+++ default.R
@@ -1,11 +1,72 @@
 ### Necessities
 
+library(ggplot2)
+library(plotly)
+library(dplyr)
+
 # Data set Link: https://www.kaggle.com/datasets/faa/wildlife-strikes
 
-data <- read.csv("airstrike.csv")
+### ========================================================================###
 
-data[data == ""] <- NA
+### Data Set stuff
+### -------------------------------
+
+df <- read.csv("airstrike.csv")
+df[df == ""] <- NA
+
+### SINGLE CATE. VAR.
+### -------------------------------
+
+# Data Variable Manipulation for Plotting
+df_cateCount <- df %>% count(Flight.Phase)
+df_cateCount$Flight.Phase <- factor(df_cateCount$Flight.Phase, 
+                                    levels = df_cateCount$Flight.Phase[order(-df_cateCount$n)])
+# Single Cate. Bar Graph
+plot_ly(data = df_cateCount,
+  type = "bar",
+  x = ~Flight.Phase, y = ~n,
+  color = ~Flight.Phase
+) %>%
+  layout(
+    title = list(text = "Flight Phase when the Strike Occurred"),
+    xaxis = list(title = "Flight Phase"),
+    yaxis = list(title = "Count"),
+    showlegend = FALSE,
+    
+  # Title was too close to the top I didn't like it
+    margin = list(t = 80)
+  )
+
+### SINGLE QUAN. VAR.
+### -------------------------------
+
+plot_ly(data = df,
+        x = ~Height,
+        type = "histogram",
+        xbins = list(
+          size = 1000
+        )
+) %>%
+  layout(
+    xaxis = list(range = c(0,10000))
+  )
+
+summary(df$Speed)
 
 
-head(data)
-summary(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
